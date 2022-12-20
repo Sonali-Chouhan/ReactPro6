9ab33e2e601f696b5e7038909744c1cbe03b5605
Sonali-Chouhan <sonaliyuvasoft363@gmail.com>
2022-12-20 10:27:32 +0530

    react-6-project

--- react6pro/src/Redux/Reducer/reducer.js
+++ react6pro/src/Redux/Reducer/reducer.js
@@ -1,40 +1,37 @@
 const initialState = {
   List: [],
-  
 };
 export const reducer = function (state = initialState, action) {
   switch (action.type) {
     case "Add_Data":
-      var node=state.List
-      node.push(action.payload)
+      var node = state.List;
+      node.push(action.payload);
       localStorage.setItem("record", JSON.stringify(node));
       return {
         ...state,
-        List:node,
+        List: node,
       };
     case "login_Data":
       var message;
-      var error
+      var error;
       var data = action.payload;
       var item = localStorage.getItem("record");
       var object = JSON.parse(item);
-      console.log("44223",object)
+      console.log("44223", object);
       object.map((e) => {
         if (e.email === data.email && e.password === data.password) {
-            message = "login";
-            localStorage.setItem("Token",message)
-           } else {
-             console.error("empty")
-             error ="errors"
-           }
+          message = "login";
+          localStorage.setItem("Token", message);
+        } else {
+          console.error("empty");
+          error = "errors";
+        }
       });
       return {
         ...state,
         data: [...object],
         Logintoken: message,
-        error:error
-        
-          
+        error: error,
       };
 
     default:
